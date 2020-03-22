06c889f9be3785461a0749594b67518f37f70ae9
Steffen Hansen <mail@steffen-hansen.dk>
2020-03-22 22:47:52 +0100

    Added TODOs

--- src/paystation/domain/receipt/BarCodeAdditionalInfoPrinter.java
+++ src/paystation/domain/receipt/BarCodeAdditionalInfoPrinter.java
@@ -2,6 +2,7 @@ package paystation.domain.receipt;
 
 import java.io.PrintStream;
 
+// TODO: Should be implemented as SINGLETON
 public class BarCodeAdditionalInfoPrinter implements AdditionalInfoPrinter {
     @Override
     public void print(PrintStream printStream) {
